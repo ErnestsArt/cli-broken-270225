741f5f34aa95d9e5d60477443bb02b592da0185f
you@example.com <you@example.com>
2025-02-27 16:13:21 +0200

    job done

--- web/contact.php
+++ web/contact.php
@@ -6,6 +6,34 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="index.php" method="post">
+
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+        <br>
+
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+        <br>
+
+        <label for="level">Level</label>
+        <select name="level" id="level">
+            <option value="advanced">Advanced</option>
+            <option value="beginner">Beginner</option>
+            <option value="por">Pro</option>
+        </select>
+        <br>
+
+        <p>gribi sanemt info?</p>
+        <label for="radio">ja</label>
+        <input type="radio" name="yes" id="yes">
+        <label for="radio">ne</label>
+        <input type="radio" name="yes" id="yes">
+
+        <br>
+        <input type="submit" value="Nosūtīt">
+    </form>
+    
     <a href="index.php">Next Page</a>
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -6,6 +6,34 @@
     <title>Document</title>
 </head>
 <body>
+
+<?php
+
+$books = [
+    1 => [
+        'title' => 'The Great Gatsby',
+        'author' => 'F. Scott Fitzgerald'
+    ],
+    2 => [
+        'title' => '1984',
+        'author' => 'George Orwell'
+    ],
+    3 => [
+        'title' => 'Pride and Prejudice',
+        'author' => 'Jane Austen'
+    ]
+];
+foreach ($books as $key => $book) {
+    echo "ID: ". $key .  " // Title: ". $book['title'] . " // Author: " . $book['author']. "\n\n";
+}
+    echo "<br>";
+    echo 'forma pieteikta ar: <br>'. 'vārdu: '. htmlspecialchars($_POST["name"]);
+    echo '<br> emailu: '. htmlspecialchars($_POST["email"]);
+    echo '<br> līmeni: '. htmlspecialchars($_POST["level"]);
+    
+?>
+    
+    <br>
     <a href="contact.php">Next Page</a>
 </body>
 </html>
